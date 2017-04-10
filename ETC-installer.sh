8dca4e1d2d5ae468cb435832123fcf23a9f65de0
Yohan Graterol <yohangraterol92@gmail.com>
2017-04-10 00:38:38 -0500

    Indent sudoscript code

--- ETC-installer.sh
+++ ETC-installer.sh
@@ -23,38 +23,38 @@ if [ $CACHE_SIZE -lt 512 ]; then
 fi
 
 cat <<EOT >> $SUDO_SCRIPT
-#!/usr/bin/env bash
-set -e
-echo "Cleaning before instalation..."
-echo "Removing geth user"
-userdel geth || true  2> /dev/null
-rm -rf $HOME_GETH || true  2> /dev/null
-rm -f /etc/supervisor/conf.d/ethereum-classic.conf
+  #!/usr/bin/env bash
+  set -e
+  echo "Cleaning before instalation..."
+  echo "Removing geth user"
+  userdel geth || true  2> /dev/null
+  rm -rf $HOME_GETH || true  2> /dev/null
+  rm -f /etc/supervisor/conf.d/ethereum-classic.conf
 
-useradd -m -U geth
-add-apt-repository -y ppa:longsleep/golang-backports
-apt-get update
-apt-get upgrade -y
-apt-get install -y git build-essential software-properties-common golang-go supervisor
-runuser -l geth -c "mkdir -p $ETC_DIRECTORY && cd $ETC_DIRECTORY && git clone https://github.com/ethereumproject/go-ethereum.git"
-runuser -l geth -c "cd $ETC_DIRECTORY/go-ethereum && go get -t -v ./... && go build ./cmd/geth && cp geth $HOME_GETH && cd $HOME_GETH && rm -rf go"
-mv /home/geth/geth /usr/bin/
+  useradd -m -U geth
+  add-apt-repository -y ppa:longsleep/golang-backports
+  apt-get update
+  apt-get upgrade -y
+  apt-get install -y git build-essential software-properties-common golang-go supervisor
+  runuser -l geth -c "mkdir -p $ETC_DIRECTORY && cd $ETC_DIRECTORY && git clone https://github.com/ethereumproject/go-ethereum.git"
+  runuser -l geth -c "cd $ETC_DIRECTORY/go-ethereum && go get -t -v ./... && go build ./cmd/geth && cp geth $HOME_GETH && cd $HOME_GETH && rm -rf go"
+  mv /home/geth/geth /usr/bin/
 
-apt-get remove -y golang-go git
+  apt-get remove -y golang-go git
 
-cat <<EOT >> /etc/supervisor/conf.d/ethereum-classic.conf
-[program:ethereum-classic]
-user=geth
-directory=/home/geth
-command=/usr/bin/geth --fast --cache=$CACHE_SIZE
-autostart=true
-autorestart=true
-stderr_logfile=/var/log/geth.err.log
-stdout_logfile=/var/log/geth.out.log
-systemctl enable supervisor
-systemctl start supervisor
-supervisorctl update
-EOT
+  cat <<EOT >> /etc/supervisor/conf.d/ethereum-classic.conf
+  [program:ethereum-classic]
+  user=geth
+  directory=/home/geth
+  command=/usr/bin/geth --fast --cache=$CACHE_SIZE
+  autostart=true
+  autorestart=true
+  stderr_logfile=/var/log/geth.err.log
+  stdout_logfile=/var/log/geth.out.log
+  systemctl enable supervisor
+  systemctl start supervisor
+  supervisorctl update
+  EOT
 EOT
 
 sudo bash $SUDO_SCRIPT
